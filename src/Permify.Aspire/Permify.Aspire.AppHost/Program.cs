eb21d6b7a517e21d7ff6c702119779bc12ef96ea
Yan Pitangui <yanpitangui@gmail.com>
2023-12-24 15:14:21 -0300

    UP: Added otel collector but for some reason traces don't correlate between aspnetcore grpc call and permify traces

--- src/Permify.Aspire/Permify.Aspire.AppHost/Program.cs
+++ src/Permify.Aspire/Permify.Aspire.AppHost/Program.cs
@@ -8,14 +8,25 @@ var postgresdb = builder.AddPostgresContainer("postgres", port:5432, password:"l
     .WithVolumeMount("pg-data", "/var/lib/postgresql/data", VolumeMountType.Named)
     .AddDatabase("aspire-permify");
 
+var otelCollector = builder.AddContainer("otel-collector", "otel/opentelemetry-collector-contrib")
+    .WithServiceBinding(4318, 4318, name: "http")
+    .WithVolumeMount("../../../etc/otelcol-config.yaml", "/etc/otelcol-config.yml")
+    .WithArgs("--config=/etc/otelcol-config.yml");
+
 string? oltpEndpoint = builder.Configuration.GetValue<string>("DOTNET_DASHBOARD_OTLP_ENDPOINT_URL");
 var permify = builder.AddContainer("permify", "permify/permify", "latest")
     .WithArgs("serve")
-    // .WithEnvironment("PERMIFY_TRACER_ENABLED", "true")
-    // .WithEnvironment("PERMIFY_TRACER_EXPORTER", "otlp")
-    // .WithEnvironment("PERMIFY_TRACER_ENDPOINT", "host.docker.internal:16150")
-    // .WithEnvironment("PERMIFY_TRACER_INSECURE", "true")
-    // Aspire dashboard otlp only works with grpc endpoints
+    .WithEnvironment("PERMIFY_TRACER_ENABLED", "true")
+    .WithEnvironment("PERMIFY_TRACER_EXPORTER", "otlp")
+    // Aspire dashboard otlp only works with grpc endpoints, so we use the collector to send the traces to the dashboard
+    .WithEnvironment("PERMIFY_TRACER_ENDPOINT", "host.docker.internal:4318")
+    .WithEnvironment("PERMIFY_TRACER_INSECURE", "true")
+    
+    .WithEnvironment("PERMIFY_METER_ENABLED", "true")
+    .WithEnvironment("PERMIFY_METER_EXPORTER", "otlp")
+    // Aspire dashboard otlp only works with grpc endpoints, so we use the collector to send the metrics to the dashboard
+    .WithEnvironment("PERMIFY_METER_ENDPOINT", "host.docker.internal:4318")
+    .WithEnvironment("PERMIFY_METER_INSECURE", "true")
     // .WithEnvironment("PERMIFY_DATABASE_ENGINE", "postgres")
     // .WithEnvironment("PERMIFY_DATABASE_AUTO_MIGRATE", "true")
     // .WithEnvironment("PERMIFY_DATABASE_AUTO_MIGRATE", "true")
